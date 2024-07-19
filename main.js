570650d591aa279fc3147382fc8605c9f4e0a7ba
Nikhil <76193564+Ryogami-Dev@users.noreply.github.com>
2024-07-19 15:04:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,60 @@
-//JAVAscript
-hello
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d")
+
+
+let checkerBoard = [     
+
+    ["", "red", "", "red", "", "red", "", "red"],
+    ["red", "", "red", "", "red", "", "red", ""],
+    ["", "red", "", "red", "", "red", "", "red"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["gray", "", "gray", "", "gray", "", "gray", ""],
+    ["", "gray", "", "gray", "", "gray", "", "gray"],
+    ["gray", "", "gray", "", "gray", "", "gray", ""]
+
+]   
+
+function drawBoard(){
+    
+    for(let i = 0 ; i < 8 ; i++){
+        for(let j = 0 ; j < 8 ; j++){
+            if((i+j) % 2 == 0 ){         
+                ctx.fillStyle = "White"
+            }else{
+                ctx.fillStyle = "Black"
+            }
+            ctx.fillRect(i*100,j*100,100,100) 
+        }
+    }
+
+}  
+
+
+function drawPieces(){
+
+    for(let i = 0 ; i < 8 ; i++){
+        for(let j = 0 ; j < 8 ; j++){
+            if(checkerBoard[i][j] !== ""){
+                
+                ctx.beginPath();
+                ctx.arc(j*100+50 , i*100+50 ,40,0, 2*Math.PI)
+                if(checkerBoard[i][j] == "red"){
+                    ctx.fillStyle = "red";
+                }else{
+                    ctx.fillStyle = "gray"
+                }
+
+                ctx.fill();
+                ctx.closePath()
+
+            }
+        }
+    }
+
+}
+
+window.addEventListener("load", function(){
+    drawBoard();
+    drawPieces();
+})
